5d38352b5d888cdd2ae8e474253cc66043b58c1f
FreezyAce <saskia@list.ru>
2022-10-24 18:48:47 +0400

    Create impl files. New annotation.

--- src/main/java/web/dao/DaoUserImpl.java
+++ src/main/java/web/dao/DaoUserImpl.java
@@ -1,15 +1,14 @@
 package web.dao;
 
-
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
-@Component
-public class DaoUserImpl {
+@Repository
+public class DaoUserImpl implements DaoUser {
 
     @PersistenceContext
     private EntityManager entityManager;
